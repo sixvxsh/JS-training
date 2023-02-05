89eac0b6464a4430b11f3b076c8bed5f630c10de
sixvxsh <sixvxsh.j@gmail.com>
2023-02-05 19:01:11 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -255,6 +255,20 @@ const ORDER_TYPES = {
     BURGER: 2,
     
 }
+
+// --------------------------------------
+const ORDER_TYPES = require('./orderTypes');
+
+function numberOfPizzas(orders) {
+    let total = 0;
+    for(let i = 0; i < orders.length; i++) {
+        if (orders[i].type === ORDER_TYPES.PIZZA) {
+            total += orders[i].pizzas;
+        }
+    }
+    return total;
+}
+
 // --------------------------------------
 
 
