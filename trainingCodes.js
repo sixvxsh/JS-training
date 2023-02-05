754ded60f638982213c3fcd7601343ef6eee4536
sixvxsh <sixvxsh.j@gmail.com>
2023-02-05 13:31:09 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -213,5 +213,13 @@ function addOne(array) {
     }
 }
 // --------------------------------------
+function removeOccurrences(array, num) {
+    for(let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === num) {
+            array.splice(i , 1);
+        }
+    }
+}
+// --------------------------------------
 
 
