bc438c36781e726c9b362a80a8497facffd99eb8
sixvxsh <sixvxsh.j@gmail.com>
2023-02-07 16:10:50 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -412,3 +412,12 @@ function playerHandScore(hand) {
 }
 
 // --------------------------------------
+
+function willEat(hasPizza, hasDonuts, hasCookies) {
+    if (hasPizza || hasDonuts || hasCookies ) {
+        return true;
+    }
+    return false;
+}
+
+// --------------------------------------
