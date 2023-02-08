f69b5faf96cc434789f7c390c0bec47955ed6397
sixvxsh <sixvxsh.j@gmail.com>
2023-02-08 18:34:11 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -528,3 +528,19 @@ function eitherNotBoth(num) {
 
 // --------------------------------------
 
+function fizzBuzz(numbers) {
+    let str = '';
+    for(let i = 0; i < numbers.length; i++) {
+        const number = numbers[i]
+        if (number % 3 === 0) {
+            str += 'fizz';
+        }
+        if (number % 5 === 0) {
+            str += 'buzz';
+        }
+    }
+    return str;
+}
+
+
+// --------------------------------------
