2df2efcbbc138d90e5124413876c1817eae98429
sixvxsh <sixvxsh.j@gmail.com>
2023-02-13 19:40:34 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -926,7 +926,16 @@ function largest(numbers) {
 
 
 // --------------------------------------
+// numbers is an array full of numbers
+// let's find the positive largest and return it
+// i.e. [2,3,5,1,4] => 5
+function largest(numbers) {
+    return numbers.reduce((accumulator, currentValue) => {
+        return accumulator > currentValue ? accumulator:currentValue;
+    }, 1);
+}
 
+// --------------------------------------
 
 
 
