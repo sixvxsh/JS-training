74cf72191a58d10f1ee526161636e54ca1e1d877
sixvxsh <sixvxsh.j@gmail.com>
2023-02-13 19:20:05 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -904,6 +904,30 @@ function unique(array) {
 
 // --------------------------------------
 
+// numbers is an array full of numbers
+// let's add all the numbers and return the sum
+// i.e. [1,2,3,4,5] => 15
+function sum(numbers) {
+    return numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    })
+}
+
+// --------------------------------------
+
+// numbers is an array full of numbers
+// let's find the largest and return it
+// i.e. [2,3,5,1,4] => 5
+function largest(numbers) {
+    return numbers.reduce((accumulator, currentValue) => {
+        return ( accumulator > currentValue ? accumulator:currentValue);
+    })
+}
+
+
+// --------------------------------------
+
+
 
 
 
