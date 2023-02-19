7b6c19eabee30abd0f9740071cd71af9ff982dd1
sixvxsh <sixvxsh.j@gmail.com>
2023-02-19 13:44:09 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -1119,3 +1119,38 @@ module.exports = OperationManager;
 
 
 
+const Stack = require('./Stack');
+
+class OperationManager {
+    constructor() {
+        this.operations = new Stack();
+        this.undos = new Stack(); 
+    }
+
+    addOperation(operation) {
+        this.operations.push(operation);
+
+    }
+
+    undo() {
+        const operation = this.operations.pop();
+        this.undos.push(operation);
+    }
+
+    redo() {
+        const operation = this.undos.pop();
+        this.operations.push(operation);
+    }
+
+    redoAll() {
+        while(!this.undos.isEmpty()) {
+            this.redo();
+        }
+    }
+}
+
+module.exports = OperationManager;
+
+
+// --------------------------------------
+
