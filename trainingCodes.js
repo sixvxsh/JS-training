eb34ce5d17cbef96150e7d9ae84e384c5d06d90d
sixvxsh <sixvxsh.j@gmail.com>
2023-02-19 12:32:24 +0330

    Updated

--- trainingCodes.js
+++ trainingCodes.js
@@ -969,6 +969,31 @@ function walk(node) {
 // --------------------------------------
 
 
+const { MAX_STACK_SIZE } = require('./config');
+
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        this.items.push(item);
+    }
+    pop() {
+        return this.items.pop();
+    }
+    isEmpty() {
+        
+    }
+    peek() {
+        
+    }
+}
+
+module.exports = Stack;
+
+// --------------------------------------
+
+
 
 
 
